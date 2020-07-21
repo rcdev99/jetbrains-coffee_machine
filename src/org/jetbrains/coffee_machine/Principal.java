58d3a0ec325755ce1058ca3983f6f96e8cc85428
rcdev99 <ricardo06.rodrigues@gmail.com>
2020-07-21 10:34:13 -0300

    Etapa 1/6: Exibição do processo de preparação do café.

---
+++ src/org/jetbrains/coffee_machine/Principal.java
@@ -0,0 +1,14 @@
+package org.jetbrains.coffee_machine;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		System.out.println("Iniciando o preparo ...");
+        System.out.println("Moendo os grãos de café");
+        System.out.println("Fervendo a água");
+        System.out.println("Misturando a água fervida com os grãos de café");
+        System.out.println("Servindo o café na xícara");
+        System.out.println("Colocando leite no copo");
+        System.out.println("Seu café está pronto");
+	}
+}
