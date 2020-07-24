4ddbf7fe644a4b647a906663787946e12247bec2
rcdev99 <ricardo06.rodrigues@gmail.com>
2020-07-24 15:02:32 -0300

    feat: Etapa 3 - Café suficiente para todos.

--- src/org/jetbrains/coffee_machine/Principal.java
+++ src/org/jetbrains/coffee_machine/Principal.java
@@ -6,31 +6,71 @@ public class Principal {
 
 	public static void main(String[] args) {
 		
-		//Variáveis necessárias
-		Scanner scanner = new Scanner(System.in);
-		//Inicializando sistema
-		System.out.println("Seja bem-vindo a sua Coffe-Machine !");
-		System.out.println("\nInforme quantas xícaras de café deseja: ");
-		int qtdXicaras = scanner.nextInt();
-		scanner.close();
-		System.out.println(infoQtdIngredientesNecessario(qtdXicaras));
+		//Required variables
+	     Scanner scanner = new Scanner(System.in);
+	     //Data input
+	     System.out.println("Informe a quantidade de água disponível (ml): ");
+	     Integer qtdWater = scanner.nextInt();
+	     System.out.println("Informe a quantidade de leite disponível (ml): ");
+	     Integer qtdMilk = scanner.nextInt();
+	     System.out.println("Informe a quantidade de grãos de café disponíveis (g): ");
+	     Integer qtdCoffee = scanner.nextInt();
+	     System.out.println("Informe quantas xícaras de café deseja: ");
+	     Integer qtdCupsNeeded = scanner.nextInt();
+	     //How many can we produce ?
+	     Integer qtdCanBeProduce = howMuchCanIProduce(qtdWater, qtdMilk, qtdCoffee);
+	     if (qtdCupsNeeded > qtdCanBeProduce) {
+	         System.out.println("Desculpe, atualmente posso preparar apenas " + qtdCanBeProduce + " café(s)");
+	     } else if (qtdCupsNeeded < qtdCanBeProduce) {
+	         System.out.println("Sim, eu posso preparar a quantidade solicitada (ou até mais " + (qtdCanBeProduce - qtdCupsNeeded) + ", caso deseje)");
+	     } else {
+	         System.out.println("Sim, eu posso preparar esta quantidade.");
+	     }
+	     scanner.close();
 	}
 	
 	/**
-	 * Função utilizada para exibir informações sobre a quantidade de ingredientes necessários para a preparação de um dado
-	 * número de xícaras de café
-	 * @return
-	 * @param int qtdXicaras número de xícaras a serem preparadas
+	 * Function used to display information about the amount of ingredients needed to prepare a given number 
+	 * of cups of coffee
+	 * @param int qtdXicaras number of coffee cups to be prepared
+	 * @return Text containing the amount of ingredients needed
 	 */
-	public static String infoQtdIngredientesNecessario(int qtdXicaras) {
-		//Instanciando variável que conterá o texto do resualtado
+	public static String infoQttNecessaryIngredients(int qttCups) {
+		//variable for text concatenation
 		StringBuilder sb = new StringBuilder();
-		//Construindo texto do resultado
-		sb.append("Para preparar " + qtdXicaras + " xícaras de café são necessários : ");
-		sb.append("\n - " + qtdXicaras * 200 + " ml de água");
-		sb.append("\n - " + qtdXicaras * 50 + " ml de leite");
-		sb.append("\n - " + qtdXicaras * 15 + " g de grãos de café");
-		//Retornando informação sobre a quantiade necessária de ingredientes
+		//building result text
+		sb.append("Para preparar " + qttCups + " xícaras de café são necessários : ");
+		sb.append("\n - " + qttCups * 200 + " ml de água");
+		sb.append("\n - " + qttCups * 50 + " ml de leite");
+		sb.append("\n - " + qttCups * 15 + " g de grãos de café");
+		//returning concatenated text
 		return sb.toString();
 	}
+	
+	/**
+     * Method used to calculate the amount of coffee the machine can brew
+     * @param water amount of ml water the machine has
+     * @param milk amount of ml milk the machine has
+     * @param coffee amount of g of coffe beans the machine has
+     * @return qtdCanBeProduce amount of cups that can be produced
+     */
+    public static Integer howMuchCanIProduce(int water, int milk, int coffee){
+        //Local variables
+        Integer qtdCanBeProduce = 0;
+        boolean canIProduce = true;
+        //Processing
+        do {
+            water -= 200; //amount of water needed per cup
+            milk -= 50; //amount of milk needed per cup
+            coffee -= 15; //amount of coffee needed per cup
+            //Can i produce ?
+            if (water >= 0 && milk >= 0 && coffee >= 0) {
+                qtdCanBeProduce++;
+            }else{
+                canIProduce = false;
+            }
+        } while (canIProduce);
+        //Returns
+        return qtdCanBeProduce;
+    }
 }
